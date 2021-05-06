734d7b1e2c716a156f859aa3ab988a5857c22ccb
Unk3wn <holzhaeuser.nico@gmail.com>
2021-05-06 21:23:09 +0200

    :sparkles: Moved Method to Rental.java

--- src/main/java/de/se/refactoring/Rental.java
+++ src/main/java/de/se/refactoring/Rental.java
@@ -16,4 +16,25 @@ class Rental {
     public Movie getMovie() {
         return movie;
     }
+
+    //One Commit to Far, so first Step was already DONE :)
+    public double getCharge() {
+        double rentalAmount = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                rentalAmount += 2;
+                if (getDaysRented() > 2)
+                    rentalAmount += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                rentalAmount += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                rentalAmount += 1.5;
+                if (getDaysRented() > 3)
+                    rentalAmount += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return rentalAmount;
+    }
 }
